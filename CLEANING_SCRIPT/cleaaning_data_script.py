d439adde3febdf9d4e0cce4e065c960d77abc766
akakiusz <101632902+Akakiusz@users.noreply.github.com>
2026-08-31 09:34:42 +0100

    Update cleaaning_data_script.py
    cleaning data process ,source implemented

--- CLEANING_SCRIPT/cleaaning_data_script.py
+++ CLEANING_SCRIPT/cleaaning_data_script.py
@@ -4,3 +4,9 @@ import pandas as pd
  
 INPUT_FILE = "Tweets.csv"
 OUTPUT_FILE = "processed_tweets.csv"
+
+""" This script reads the input CSV file, processes the data, and saves the cleaned data to a new CSV file. """
+def main():
+    # Load the dataset
+    df = pd.read_csv(INPUT_FILE)
+

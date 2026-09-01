1af520634639e3e1178e0c65455d6df9867d94e6
akakiusz <101632902+Akakiusz@users.noreply.github.com>
2026-09-01 10:27:54 +0100

    Update cleaaning_data_script.py
    directories for datasets amended

--- CLEANING_SCRIPT/cleaaning_data_script.py
+++ CLEANING_SCRIPT/cleaaning_data_script.py
@@ -7,8 +7,8 @@
  
 import pandas as pd
  
-INPUT_FILE = "../DATASETS/Tweets.csv"
-OUTPUT_FILE = "../DATASETS/processed_tweets.csv"
+INPUT_FILE = "DATASETS/Tweets.csv"
+OUTPUT_FILE = "DATASETS/processed_tweets.csv"
  
  
 def main():
